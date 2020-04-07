0890dd0d1fded3eb5930ca85afd9d5b4dbb8cea2
Toni Barjas <tonibarjasmartinez@gmail.com>
2020-04-08 01:53:55 +0200

    Second commit

---
+++ project-6/migrations/2_deploy_contracts (2).js
@@ -0,0 +1,16 @@
+// migrating the appropriate contracts
+var FarmerRole = artifacts.require("./FarmerRole.sol");
+var DistributorRole = artifacts.require("./DistributorRole.sol");
+var RetailerRole = artifacts.require("./RetailerRole.sol");
+var ConsumerRole = artifacts.require("./ConsumerRole.sol");
+var SupplyChain = artifacts.require("./SupplyChain.sol");
+var Ownable = artifacts.require('./Ownable.sol');
+
+module.exports = function (deployer, network, accounts) {  
+    deployer.deploy(SupplyChain, { from: accounts[0] });
+    deployer.deploy(FarmerRole, { from: accounts[1] });
+    deployer.deploy(DistributorRole, { from: accounts[2] });
+    deployer.deploy(RetailerRole, { from: accounts[3] } );
+    deployer.deploy(ConsumerRole, { from: accounts[4] });
+    deployer.deploy(Ownable);
+};
